15ed516624cb277c03d172686af382499d2f1b45
procodingclass <88050205+procodingclass@users.noreply.github.com>
2023-07-21 14:11:57 +0530

    Update cipher.py

--- SA1/cipher.py
+++ SA1/cipher.py
@@ -2,20 +2,13 @@ capitalLetters='ABCDEFGHIJKLMNOPQRSTUVWXYZ'
 lowerLetters='abcdefghijklmnopqrstuvwxyz'
 numbers='0123456789'
 
-# Define the function cipher() that takes two parameter i.e 'plaintext' and a key 'n'
 def cipher(plaintext, n):
-    # Access globals: capitalLetters, lowerLetters, numbers
     global  capitalLetters, lowerLetters, numbers
-    # Create empty string variable cipherText
     cipherText = ''
 
-    # Loop throught each 'char' in the 'plaintext'
     for char in plaintext:
-        # Check if 'char' is present in 'numnbers' list
         if char in numbers:
-            # Get the current position of the 'char' in 'numbers' list
             currentPosition = numbers.find(char)
-            # Calculate the character using formula : numbers[(currentPosition + n ) % 10] and add it to cipherText
             cipherText += numbers[(currentPosition + n ) % 10]
         # Check if 'char' is present in 'lowerLetters' list
         elif char in lowerLetters:
@@ -29,10 +22,8 @@ def cipher(plaintext, n):
             currentPosition = capitalLetters.find(char)
             # Calculate the character using formula : numbers[(currentPosition + n ) % 26] and add it to cipherText
             cipherText += capitalLetters[(currentPosition + n) % 26] 
-        # Else add the 'char' to 'cipherText'
         else:
             cipherText += char
     
-    # Return 'cipherText'
     return cipherText
         
