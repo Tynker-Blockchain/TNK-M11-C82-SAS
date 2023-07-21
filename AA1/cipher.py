83cd1f240f3097b08819f4dd16ea549ceea64c67
procodingclass <88050205+procodingclass@users.noreply.github.com>
2023-07-21 14:35:33 +0530

    Update cipher.py

--- AA1/cipher.py
+++ AA1/cipher.py
@@ -1,30 +1,19 @@
-capitalLetters='ABCDEFGHIJKLMNOPQRSTUVWXYZ'
-lowerLetters='abcdefghijklmnopqrstuvwxyz'
-numbers='0123456789'
-# Create string of special characters
-specials = '!@#$%^&*()}{\/?'
+# Create a list AllCharacters
+AllCharacters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789!@#$%^&*()}{\/?'
 
 def cipher(plaintext, n):
-    # Access specials as global
-    global  capitalLetters, lowerLetters, numbers, specials
+    # Access AllCharacters as global
+    global  AllCharacters
     ciphertext = ''
 
     for char in plaintext:
-        if char in numbers:
-            currentPosition = numbers.find(char)
-            ciphertext += numbers[(currentPosition + n ) % 10]
-        elif char in lowerLetters:
-            currentPosition = lowerLetters.find(char)
-            ciphertext += lowerLetters[(currentPosition + n )% 26] 
-        elif char in capitalLetters:
-            currentPosition = capitalLetters.find(char)
-            ciphertext += capitalLetters[(currentPosition + n) % 26]
-        # Check if char exits in specials  
-        elif char in specials:
-            # Get position of the char in specials   
-            currentPosition = specials.find(char)
-            # Get the new cha using formula : specials[(currentPosition + n) % 15], and add it to ciphertext
-            ciphertext += specials[(currentPosition + n) % 15]
+        # Check if char exits in AllCharacters
+        if char in AllCharacters:
+            # Find position of char in AllCharacters
+            currentPosition = AllCharacters.find(char)
+            # Calculate the cipher text as AllCharacters[(currentPosition + n ) % 77]
+            ciphertext += AllCharacters[(currentPosition + n ) % 77]
+        # Remove rest of the elif's
         else:
             ciphertext += char
 
@@ -32,25 +21,17 @@ def cipher(plaintext, n):
         
 def decipher(ciphertext, n):
     # Access specials as global
-    global capitalLetters, lowerLetters, numbers, specials
+    global AllCharacters
     plaintext = ""
 
     for char in ciphertext:
-        if char in numbers:
-            currentPosition = numbers.find(char)
-            plaintext += numbers[(currentPosition - n) % 10]
-        elif char in lowerLetters:
-            currentPosition = lowerLetters.find(char)
-            plaintext += lowerLetters[(currentPosition - n )% 26] 
-        elif char in capitalLetters:
-            currentPosition = capitalLetters.find(char)
-            plaintext += capitalLetters[(currentPosition - n) % 26] 
-        # Check if char exits in specials  
-        elif char in specials:    
-            # Get position of the char in specials 
-            currentPosition = specials.find(char)
-            # Get the new cha using formula : specials[(currentPosition - n) % 15], and add it to plaintext
-            plaintext += specials[(currentPosition - n) % 15]         
+        # Check if char exits in AllCharacters
+        if char in AllCharacters:
+            # Find position of char in AllCharacters
+            currentPosition = AllCharacters.find(char)
+            # Calculate the cipher text as AllCharacters[(currentPosition - n ) % 77]
+            plaintext += AllCharacters[(currentPosition - n) % 77]
+        # Remove rest of the elif's
         else:
             plaintext += char
             
